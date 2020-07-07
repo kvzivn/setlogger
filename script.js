540f3941436615e64c79c9d229ee59063f7123e2
kvzivn <hello@kevinivan.com>
2020-07-07 23:25:29 +0200

    fix script

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-var exerciseInput = document.querySelector(".js-exercise");
+const exerciseInput = document.getElementById("js-exercise");
 const setsInput = document.querySelector(".js-sets");
 const repsInput = document.querySelector(".js-reps");
 const addBtn = document.querySelector(".js-addBtn");
 
-//addBtn.addEventListener("click", () => {
-//console.log("click")
-//})
-console.log(exerciseInput);
+console.log(addBtn);
