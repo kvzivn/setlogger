05f51465d374023e0c8720caa20ef2fce0edade9
Benjamin <Tsubarah@hotmail.se>
2020-07-07 23:19:30 +0200

    lol

---
+++ script.js
@@ -0,0 +1,9 @@
+var exerciseInput = document.querySelector(".js-exercise");
+const setsInput = document.querySelector(".js-sets");
+const repsInput = document.querySelector(".js-reps");
+const addBtn = document.querySelector(".js-addBtn");
+
+//addBtn.addEventListener("click", () => {
+//console.log("click")
+//})
+console.log(exerciseInput);
