2c27b19b9646f4974c43ff67d6734553e573072e
Benjamin <Tsubarah@hotmail.se>
2020-07-08 00:00:26 +0200

    Uppdaterad

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const exerciseInput = document.getElementById("js-exercise");
-const setsInput = document.querySelector(".js-sets");
-const repsInput = document.querySelector(".js-reps");
-const addBtn = document.querySelector(".js-addBtn");
+const exerciseInput = document.querySelector(".js-exercise")
+const setsInput = document.querySelector(".js-sets")
+const repsInput = document.querySelector(".js-reps")
+const addBtn = document.querySelector(".js-addBtn")
+const setsContainer = document.querySelector(".js-sets")
 
-console.log(addBtn);
+addBtn.addEventListener("click", () => {
+    const exercise = exerciseInput.value
+    const sets = setsInput.value 
+    const reps = repsInput.value
+    
+    const logSetString = `${exercise} ${sets} * ${reps}`
+
+    const logSetDiv = document.createElement("div")
+    const logSetNode = document.createTextNode(logSetString)
+
+    logSetDiv.appendChild(logSetNode)
+
+    setsContainer.appendChild(logSetDiv)
+})
